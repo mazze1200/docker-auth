4560cf176f96b0c32ec13f7f705b96d4ef37ce92
mazze.org <mathias.boesl@gmx.de>
2025-05-03 13:21:57 +0000

    fix password replacing

--- create_passwords.sh
+++ create_passwords.sh
@@ -2,17 +2,20 @@
 set -e
 echo "Generating passwords. Please store them securely"
 
-LLDAP_JWT_SECRET=$(openssl rand -base64 32)
+LLDAP_JWT_SECRET=$(openssl rand -base64 32 | tr -dc 'a-zA-Z0-9')
 #printf "$LLDAP_JWT_SECRET" | docker secret create LLDAP_JWT_SECRET -
-sed -i 's/"LLDAP_JWT_SECRET"/"'${LLDAP_JWT_SECRET}'"/g' lldap/data/lldap_config.toml
 
-LLDAP_KEY_SEED=$(openssl rand -base64 64)
+LLDAP_KEY_SEED=$(openssl rand -base64 32 | tr -dc 'a-zA-Z0-9')
 #printf "$LLDAP_KEY_SEED" | docker secret create LLDAP_KEY_SEED -
-sed -i 's/"LLDAP_KEY_SEED"/"'${LLDAP_KEY_SEED}'"/g' lldap/data/lldap_config.toml
 
-LLDAP_LDAP_USER_PASS=$(openssl rand -base64 32)
+LLDAP_LDAP_USER_PASS=$(openssl rand -base64 32 | tr -dc 'a-zA-Z0-9')
 #printf "$LLDAP_LDAP_USER_PASS" | docker secret create LLDAP_LDAP_USER_PASS -
-sed -i 's/"LLDAP_LDAP_USER_PASS"/"'${LLDAP_LDAP_USER_PASS}'"/g' lldap/data/lldap_config.toml
+
+sed -i -e 's/"LLDAP_JWT_SECRET"/"'"${LLDAP_JWT_SECRET}"'"/g' \
+    -e 's/"LLDAP_KEY_SEED"/"'"${LLDAP_KEY_SEED}"'"/g' \
+    -e 's/"LLDAP_LDAP_USER_PASS"/"'"${LLDAP_LDAP_USER_PASS}"'"/g' \
+    lldap/data/lldap_config.toml
+
 
 echo LLDAP_JWT_SECRET: $LLDAP_JWT_SECRET
 echo LLDAP_KEY_SEED: $LLDAP_KEY_SEED
