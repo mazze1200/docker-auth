284124ba6b6f93c87b46b15b9dd0c2dcdaa12e1c
mazze.org <mathias.boesl@gmx.de>
2025-05-03 12:30:42 +0000

    update PW script

--- create_passwords.sh
+++ create_passwords.sh
@@ -3,16 +3,19 @@ set -e
 echo "Generating passwords. Please store them securely"
 
 LLDAP_JWT_SECRET=$(openssl rand -base64 32)
-printf "$LLDAP_JWT_SECRET" | docker secret create LLDAP_JWT_SECRET -
+#printf "$LLDAP_JWT_SECRET" | docker secret create LLDAP_JWT_SECRET -
+sed -i 's/"LLDAP_JWT_SECRET"/"'${LLDAP_JWT_SECRET}'"/g' lldap/data/lldap_config.toml
 
-LLDAP_KEY=$(openssl rand -base64 64)
-printf "$LLDAP_KEY" | docker secret create LLDAP_KEY -
+LLDAP_KEY_SEED=$(openssl rand -base64 64)
+#printf "$LLDAP_KEY_SEED" | docker secret create LLDAP_KEY_SEED -
+sed -i 's/"LLDAP_KEY_SEED"/"'${LLDAP_KEY_SEED}'"/g' lldap/data/lldap_config.toml
 
 LLDAP_LDAP_USER_PASS=$(openssl rand -base64 32)
-printf "$LLDAP_LDAP_USER_PASS" | docker secret create LLDAP_LDAP_USER_PASS -
+#printf "$LLDAP_LDAP_USER_PASS" | docker secret create LLDAP_LDAP_USER_PASS -
+sed -i 's/"LLDAP_LDAP_USER_PASS"/"'${LLDAP_LDAP_USER_PASS}'"/g' lldap/data/lldap_config.toml
 
 echo LLDAP_JWT_SECRET: $LLDAP_JWT_SECRET
-echo LLDAP_KEY: $LLDAP_KEY
+echo LLDAP_KEY_SEED: $LLDAP_KEY_SEED
 echo LLDAP_LDAP_USER_PASS: $LLDAP_LDAP_USER_PASS
 
 echo finished.
